0242c4737a9ebd25231c1385061868d814d26586
fadsxd <mickyfads5@gmail.com>
2019-03-10 11:51:55 -0400

    agregando una clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Nombre } from './models/nombre';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyectoColegio';
+  NombreArray : Nombre[] = [
+    {id:1, nombre : "juan", country:"Bolivia"}
+  ]
 }
